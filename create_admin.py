9adc0b7712a2d8e446b04c4572e4d35c2aebe2e5
Paras <paras451@gmail.com>
2026-01-28 12:06:39 +0530

    adding new file

---
+++ create_admin.py
@@ -0,0 +1,18 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gotomarketappbackend.settings')
+django.setup()
+
+from django.contrib.auth.models import User
+
+# Details yahan bharein
+username = 'paras_singh' 
+email = 'paras@gmail.com'
+password = 'Market@14'
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print(f"Superuser '{username}' created successfully!")
+else:
+    print(f"Superuser '{username}' already exists.")
