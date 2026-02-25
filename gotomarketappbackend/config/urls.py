f492dc92a2cfc9180da92e404ed4bfce7801f8ea
PARAS SINGH CHILWAL <paraschilwal4@gmail.com>
2026-02-25 14:35:36 +0530

    Add cron job endpoint to URLs

--- gotomarketappbackend/config/urls.py
+++ gotomarketappbackend/config/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
     path("api/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/register/", register_user, name="register"),
+    path("api/cron-job/", cron_job , name="cron_job"),
 ]
 
